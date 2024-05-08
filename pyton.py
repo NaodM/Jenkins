a033920574d26531daefffa280126d777a9a00ae
NaodM <naodm2003@gmail.com>
2024-05-08 17:29:58 -0400

    python-script

---
+++ pyton.py
@@ -0,0 +1,6 @@
+sum_numbers = sum(range(1, 11))
+print("Sum of the first 10 positive integers:", sum_numbers)
+if sum_numbers % 2 == 0:
+    print("The sum is even.")
+else:
+    print("The sum is odd.")
